0491f17bf900a140a20e04d46eba27bb05c586dd
Fusezion <fusezionstream@gmail.com>
2025-06-22 02:47:14 -0400

    Make armor stand listener actually work

--- src/main/kotlin/me/lyric/asa/ArmorStandListener.kt
+++ src/main/kotlin/me/lyric/asa/ArmorStandListener.kt
@@ -1,6 +1,7 @@
 package me.lyric.asa
 
 import org.bukkit.entity.ArmorStand
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntitySpawnEvent
 
@@ -9,6 +10,7 @@ class ArmorStandListener : Listener {
 	private val config: ArmorStandConfig
 		get() = ArmorStandPlugin.armorStandConfig
 
+	@EventHandler(ignoreCancelled = true)
 	fun onArmorStandSpawn(event: EntitySpawnEvent) {
 		if (event.entity is ArmorStand && config.armsEnabled)
 			(event.entity as ArmorStand).setArms(true)
